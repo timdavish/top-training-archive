4545be7768e3868788ac790ffbe021dd9259efd5
Tim Davis <timdavish@gmail.com>
2017-02-12 14:13:40 -0800

    Changed: .gitignore
    - Added bower_components to .gitignore
    - Added node_modules to .gitignore
    - Added todo to .gitignore
    
    Removed:
    
    - bower_components from git
    - node_modules from git
    - todo from git

--- todo.js
+++
@@ -1,9 +0,0 @@
-
-var todo = {
-    sports_mongoose_model: 'separate sports model for storing sports?',
-    profiles: 'multiple profiles for student users',
-    directive: 'Zipcode directive for registering',
-
-
-    referrals: 'Implement a referral system'
-};
