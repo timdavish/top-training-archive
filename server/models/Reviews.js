5fdb7196659d615248449439758f85752b0cbc07
Tim Davis <timdavish@gmail.com>
2017-02-21 11:25:46 -0800

    Added: Review model data Closes #5 - Author ref - Trainer ref - Date posted - Rating - Content - Type

--- server/models/Reviews.js
+++ server/models/Reviews.js
@@ -2,7 +2,38 @@
 var mongoose = require('mongoose');
 
 var ReviewSchema = new mongoose.Schema({
-    
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    trainer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    rating: {
+        type: Number,
+        enum: [1, 2, 3, 4, 5],
+        default: 5,
+        required: true
+    },
+    content: {
+        type: String,
+        default: '',
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['Verified Purchase', 'Client', 'Testimonial'],
+        default: 'Client',
+        required: true
+    }
 });
 
 // Set mongoose model
