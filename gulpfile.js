4b430c801c6759777943c3835f990a23dc578cfa
Tim Davis <timdavish@gmail.com>
2017-02-26 18:38:16 -0800

    Added: Gulp
    - Started adding gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+
+// Module dependencies
+var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plug = gulpLoadPlugins(); // Auto-load plugins
+// console.log(plug);
+
+// Folders
+var folder = {
+    src: {
+        base: 'public/',
+        js: 'public/app/**/*.js',
+        html: 'public/app/**/*.ejs',
+        images: 'public/images/**/*',
+        css: 'public/styles/**/*.css'
+    },
+    dest: {
+        base: 'build/',
+        js: 'build/app/',
+        html: 'build/app/',
+        images: 'build/images/',
+        css: 'build/styles/'
+    }
+};
+
+// Development mode
+var devBuild = (process.env.NODE_ENV !== 'production') || true;
+plug.util.log('Building for', devBuild ? 'development' : 'production');
+
+// default task
+gulp.task('default', ['run', 'watch']);
+
+// run task
+gulp.task('run', ['images', 'html', 'js', 'css']);
+
+// watch task
+gulp.task('watch', function() {
+    // image changes
+    gulp.watch(folder.src.images, ['images']);
+
+    // html changes
+    gulp.watch(folder.src.html, ['html']);
+
+    // js changes
+    gulp.watch(folder.src.js, ['js']);
+
+    // css changes
+    gulp.watch(folder.src.css, ['css']);
+});
+
+// images task
+gulp.task('images', function() {
+    var src = folder.src.images;
+    var dest = folder.dest.images;
+
+    return gulp.src(src)
+        .pipe(plug.size({showFiles: false}))
+        .pipe(plug.newer(dest))
+        .pipe(plug.imagemin({ optimizationLevel: 5}))
+        .pipe(plug.size({showFiles: false}))
+        .pipe(gulp.dest(dest));
+});
+
+// html task
+gulp.task('html', function() {
+    var src = folder.src.html;
+    var dest = folder.dest.html;
+
+    return gulp.src(src)
+        .pipe(plug.size({showFiles: false}))
+        .pipe(plug.newer(dest))
+        .pipe(devBuild ? plug.util.noop() : plug.htmlclean())
+        .pipe(plug.size({showFiles: false}))
+        .pipe(gulp.dest(dest));
+});
+
+// js task
+gulp.task('js', function() {
+    var src = folder.src.js;
+    var dest = folder.dest.js;
+
+    return gulp.src(src)
+        .pipe(plug.size({showFiles: false}))
+        .pipe(plug.deporder())
+        .pipe(devBuild ? plug.util.noop() : plug.stripdebug())
+        .pipe(devBuild ? plug.util.noop() : plug.uglify())
+        .pipe(devBuild ? plug.util.noop() : plug.concat('main.js'))
+        .pipe(plug.size({showFiles: false}))
+        .pipe(gulp.dest(dest));
+});
+
+// css task
+gulp.task('css', function() {
+    var src = folder.src.css;
+    var dest = folder.dest.css;
+
+    return gulp.src(src)
+        .pipe(plug.size({showFiles: false}))
+        .pipe(devBuild ? plug.util.noop() : plug.cleanCss({}))
+        .pipe(devBuild ? plug.util.noop() : plug.concat('main.css'))
+        .pipe(plug.size({showFiles: false}))
+        .pipe(gulp.dest(dest));
+
+});
