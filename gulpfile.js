8b0b2fb93d40975b3f069518781b14c7f4b300ae
Tim Davis <timdavish@gmail.com>
2017-02-26 22:27:22 -0800

    Added: More tasks
    - nodemon for restarting server on file changes
    - del for cleaning build folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var plug = gulpLoadPlugins(); // Auto-load plugins
-// console.log(plug);
+var del = require('del');
+console.log(plug);
 
 // Folders
 var folder = {
@@ -28,10 +29,20 @@ var devBuild = (process.env.NODE_ENV !== 'production') || true;
 plug.util.log('Building for', devBuild ? 'development' : 'production');
 
 // default task
-gulp.task('default', ['run', 'watch']);
+gulp.task('default', ['run', 'serve']);
 
 // run task
 gulp.task('run', ['images', 'html', 'js', 'css']);
+
+// serve task
+gulp.task('serve', ['watch'], function() {
+    var nodemonOpts = { script: 'server/server.js' };
+
+    return plug.nodemon(nodemonOpts)
+        .on('restart', function() {
+            console.log('Nodemon', 'Server restarted');
+        });
+});
 
 // lint task
 gulp.task('lint', ['jslint', 'csslint']);
@@ -42,6 +53,7 @@ gulp.task('jslint', function() {
     var dest = folder.dest.js;
 
     return gulp.src(src)
+        .pipe(plug.plumber())
         .pipe(plug.jshint())
         .pipe(plug.jshint.reporter());
 });
@@ -52,6 +64,7 @@ gulp.task('csslint', function() {
     var dest = folder.dest.css;
 
     return gulp.src(src)
+        .pipe(plug.plumber())
         .pipe(plug.csslint())
         .pipe(plug.csslint.formatter());
 });
@@ -77,6 +90,7 @@ gulp.task('images', function() {
     var dest = folder.dest.images;
 
     return gulp.src(src)
+        .pipe(plug.plumber())
         .pipe(plug.size({showFiles: false}))
         .pipe(plug.newer(dest))
         .pipe(plug.imagemin({ optimizationLevel: 5}))
@@ -90,6 +104,7 @@ gulp.task('html', function() {
     var dest = folder.dest.html;
 
     return gulp.src(src)
+        .pipe(plug.plumber())
         .pipe(plug.size({showFiles: false}))
         .pipe(plug.newer(dest))
         .pipe(devBuild ? plug.util.noop() : plug.htmlclean())
@@ -103,6 +118,7 @@ gulp.task('js', function() {
     var dest = folder.dest.js;
 
     return gulp.src(src)
+        .pipe(plug.plumber())
         .pipe(plug.size({showFiles: false}))
         .pipe(plug.deporder())
         .pipe(devBuild ? plug.util.noop() : plug.stripdebug())
@@ -118,10 +134,18 @@ gulp.task('css', function() {
     var dest = folder.dest.css;
 
     return gulp.src(src)
+        .pipe(plug.plumber())
         .pipe(plug.size({showFiles: false}))
         .pipe(devBuild ? plug.util.noop() : plug.cleanCss({}))
         .pipe(devBuild ? plug.util.noop() : plug.concat('main.css'))
         .pipe(plug.size({showFiles: false}))
         .pipe(gulp.dest(dest));
 
+});
+
+// clean task
+gulp.task('clean', function() {
+    var dest = folder.dest.base;
+
+    return del(dest);
 });
