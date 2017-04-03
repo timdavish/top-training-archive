11fe65a8b096216a124bb15267f4535d33728fa7
Tim Davis <timdavish@gmail.com>
2017-04-03 05:04:16 -0700

    Support: contact auto-fill
    - Added auto-filling for the contact form fields if the user is logged
    in

--- client/app/modules/support/contact/contact.controller.js
+++ client/app/modules/support/contact/contact.controller.js
@@ -9,14 +9,14 @@
         .module('support.contact')
         .controller('ContactController', ContactController);
 
-    ContactController.$inject = ['$q', 'support', 'logger'];
+    ContactController.$inject = ['$q', 'authentication', 'support', 'logger'];
 
     /**
      * @namespace ContactController
      * @desc Contact controller
      * @memberof Controllers
      */
-    function ContactController($q, support, logger) {
+    function ContactController($q, authentication, support, logger) {
         var vm = this;
 
 		vm.sendRequest = sendRequest;
@@ -32,7 +32,9 @@
          */
 		 function activate() {
  			// Promises that need to be resolved to activate
- 			var promises = [];
+ 			var promises = [
+				setRequestParams()
+			];
 
  			return $q.all(promises)
  				.then(activateSuccess)
@@ -48,19 +50,44 @@
  		}
 
 		/**
+         * @name setRequestParams
+         * @desc Sets params for the support request form
+         * @memberof Controllers.ContactController
+         */
+		function setRequestParams() {
+			var deferred = $q.defer();
+
+			// If a user is logged in and their data exists
+			if (authentication.isLoggedIn() && authentication.currentUser()) {
+				var contact = authentication.currentUser().contact;
+
+				if (contact.firstname) {
+					vm.firstname = contact.firstname;
+				}
+				if (contact.lastname) {
+					vm.lastname = contact.lastname;
+				}
+				if (contact.email) {
+					vm.email = contact.email;
+				}
+				if (contact.phone) {
+					vm.phone = contact.phone;
+				}
+			}
+
+			// No matter what, this promise is resolved
+			deferred.resolve();
+
+			return deferred.promise;
+		}
+
+		/**
          * @name sendRequest
          * @desc Attempts to send support request
          * @memberof Controllers.ContactController
          */
 		function sendRequest() {
-			var params = {
-				firstname: vm.firstname,
-				lastname: vm.lastname,
-				email: vm.email,
-				phone: vm.phone,
-				subject: vm.subject,
-				content: vm.content
-			};
+			var params = getRequestParams();
 
 			support.sendRequest(params)
 				.then(sendRequestSuccess)
@@ -73,6 +100,22 @@
 			function sendRequestFail(error) {
 				logger.error('Failed to send support request', error);
 			}
+		}
+
+		/**
+         * @name getRequestParams
+         * @desc Gets params for the support request from the form
+         * @memberof Controllers.ContactController
+         */
+		function getRequestParams() {
+			return {
+				firstname: vm.firstname,
+				lastname: vm.lastname,
+				email: vm.email,
+				phone: vm.phone,
+				subject: vm.subject,
+				content: vm.content
+			};
 		}
     }
 })();
