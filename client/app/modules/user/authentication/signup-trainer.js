093731e3209a8a0b15acc625bab67e436eab61ca
Tim Davis <timdavish@gmail.com>
2017-05-03 17:11:29 -0700

    Add temporary data to signup-trainer
    - This will be used for testing new schemas and such
    - This will later be populated by form data user fills out

--- client/app/modules/user/authentication/signup-trainer.js
+++ client/app/modules/user/authentication/signup-trainer.js
@@ -32,6 +32,77 @@
          */
         function signUpTrainer() {
             vm.user.usertype = "trainer";
+			vm.user.sportData = {
+				sport: 'basketball',
+				locations: [
+					{
+						priority: 1,
+						formatted_address: "E Yesler Way, Seattle, WA",
+						geometry: {
+							type: "Point",
+							coordinates: [
+								-122.3,
+								47.62
+							]
+						}
+					},
+					{
+						priority: 2,
+						formatted_address: "Atlantic Ave, Seattle, WA",
+						geometry: {
+							type: "Point",
+							coordinates: [
+								-122.32,
+								47.6
+							]
+						}
+					}
+				],
+				packages: [
+					{
+						_id: 4,
+						sessions: 1,
+						slots: 1,
+						price: 30
+					},
+					{
+						_id: 5,
+						sessions: 2,
+						slots: 1,
+						price: 50
+					},
+					{
+						_id: 6,
+						sessions: 5,
+						slots: 1,
+						price: 110
+					}
+				],
+				events: [],
+				profile: {
+					rating: 0,
+					summary: "A passionate coach with a true love for baseball. Specializing in hitting, fielding, fundamental drills, and mental focus.",
+					credentials: {
+						experience: 5,
+						school: "University of Washington"
+					},
+					services: {
+						ages: [
+							"Middle School",
+							"High School"
+						],
+						positions: [
+							"Infield",
+							"Outfield"
+						],
+						specialties: [
+							"Fielding",
+							"Hitting",
+							"Base running"
+						]
+					}
+				}
+			};
             authentication.signUp(vm.user)
 				.then(signUpTrainerSuccess)
 				.catch(signUpTrainerFail);
